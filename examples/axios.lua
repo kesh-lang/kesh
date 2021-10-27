f85443c216036c9f46e9ca49e14e45fae1d1cefe
Joakim <joakim@users.noreply.github.com>
2021-10-27 19:05:53 +0200

    Update axios.lua

--- examples/axios.lua
+++ examples/axios.lua
@@ -50,5 +50,6 @@ axios([
 instance: axios.create [
     baseURL: 'https://some-domain.com/api/'
     timeout: 1000
-    headers: ['X-Custom-Header': 'foobar']
+    headers:
+        'X-Custom-Header': 'foobar'
 ]
