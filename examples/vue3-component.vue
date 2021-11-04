9f47c5d5e6db14df0777d440ab81d88d7de6b254
Joakim <joakim@users.noreply.github.com>
2021-11-05 00:16:43 +0100

    Create vue3-component.vue

---
+++ examples/vue3-component.vue
@@ -0,0 +1,33 @@
+<!-- original: https://github.com/mutoe/vue3-realworld-example-app/blob/master/src/components/AppLink.vue -->
+
+<template>
+  <router-link
+    :aria-label="props.name"
+    :to="props"
+    v-bind="attrs"
+  >
+    <slot />
+  </router-link>
+</template>
+
+<script lang="lua">
+import [#AppRouteNames]: 'src/router'
+import [defineComponent, PropType]: 'vue'
+import [#RouteParams]: 'vue-router'
+import [RouterLink]: 'vue-router'
+
+default: defineComponent [
+    name: 'AppLink'
+    components: [RouterLink]
+    props:
+        name:
+            type: String as PropType(#AppRouteNames)
+            required: true
+        params:
+            type: Object as PropType(#RouteParams)
+            default: () -> []
+    setup: (props, [attrs]) -> [props, attrs]
+]
+
+[default]
+</script>
