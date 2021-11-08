05b6c348d72429ee943b6c0c2a0f4046e9d9a8d5
Joakim <joakim@users.noreply.github.com>
2021-11-09 00:02:01 +0100

    Update sleep.ks.lua

--- examples/sleep.ks.lua
+++ examples/sleep.ks.lua
@@ -1,4 +1,5 @@
-sleep: async (seconds) -> Promise (resolve) -> set-timeout(resolve, seconds * 1000ms)
+sleep: async (seconds) ->
+    Promise (resolve) -> set-timeout(resolve, seconds * 1000ms)
 
 print "Hold up"
 await sleep 1s
