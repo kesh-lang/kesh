32c540bf6df0ce669602a6ec7828acd1c8d2cee9
Joakim <joakim@users.noreply.github.com>
2021-12-14 22:52:09 +0100

    Update fizzbuzz-generator.ks.lua

--- examples/fizzbuzz-generator.ks.lua
+++ examples/fizzbuzz-generator.ks.lua
@@ -1,7 +1,7 @@
 -- silly example to demonstrate the generator and imperative loop extensions
 
 fizzbuzz: (n) *=>
-    loop 1 .. n as i
+    loop 1..n as i
         yield "FizzBuzz" if i mod 15 = 0
             else "Fizz" if i mod 3 = 0
             else "Buzz" if i mod 5 = 0
