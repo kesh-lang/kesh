56842c82ddc04c53baf9a09abb44f4ac421668be
Joakim <joakim@users.noreply.github.com>
2021-12-15 00:12:07 +0100

    Create ternary.js

---
+++ transpiled/ternary.js
@@ -0,0 +1,4 @@
+// https://github.com/kesh-lang/kesh/wiki/Documentation#conditionals
+
+const inline  = age < 13 ? 'kid' : age < 20 ? 'teenager' : 'adult'
+const ternary = age < 13 ? 'kid' : age < 20 ? 'teenager' : 'adult'
