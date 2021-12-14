8929dd38ad1f87668e50a98ac790a178bebe0ed9
Joakim <joakim@users.noreply.github.com>
2021-12-14 22:48:38 +0100

    Update with.js

--- transpiled/with.js
+++ transpiled/with.js
@@ -5,7 +5,7 @@ const open = async function*(path) {
   yield file
 
   file.close()
-  console.log("file closed")
+  return console.log("file closed")
 }
 
 for await (const file of open("./hello.txt")) {
