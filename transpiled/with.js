394efc514dd8cd97de371be5ebd70ee0f2fad17d
Joakim <joakim@users.noreply.github.com>
2021-12-14 22:35:46 +0100

    Create with.js

---
+++ transpiled/with.js
@@ -0,0 +1,13 @@
+const open = async function*(path) {
+  const file = await fs.open(path)
+  console.log("file opened")
+
+  yield file
+
+  file.close()
+  console.log("file closed")
+}
+
+for await (const file of open("./hello.txt")) {
+  console.log(file.contents)
+}
