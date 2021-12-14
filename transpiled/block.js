8c9bd1317425c4587d788a19e49fd8728e11605e
Joakim <joakim@users.noreply.github.com>
2021-12-14 23:49:59 +0100

    Update block.js

--- transpiled/block.js
+++ transpiled/block.js
@@ -1,9 +1,12 @@
 // https://github.com/kesh-lang/kesh/wiki/Documentation#blocks
 
-(() => {
+const a = 1
+const answer = (() => {
   const a = 20
   const b = 22
   return a + b
 })()
 
-// 42
+answer // 42
+a      // 1
+b      // ReferenceError: b is not defined
