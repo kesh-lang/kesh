5d39e015090c40d9ee60099252aac144f5e73e4b
Joakim <joakim@users.noreply.github.com>
2021-12-17 12:31:23 +0100

    Update fibonacci.ks.lua

--- examples/fibonacci.ks.lua
+++ examples/fibonacci.ks.lua
@@ -7,3 +7,5 @@ fib-memoized: (n, memo ? *[]) ->
     if memo.{n}? then memo.{n}
     else if n < 2 then n
     else set memo.{n}: fib-memoized(n - 1, memo) + fib-memoized(n - 2, memo)
+
+[fib, fib-memoized]
