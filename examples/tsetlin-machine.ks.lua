4eb081006bd451213152eaea739c798d68651f8a
Joakim <joakim@users.noreply.github.com>
2021-12-23 20:51:28 +0100

    Update tsetlin-machine.ks.lua

--- examples/tsetlin-machine.ks.lua
+++ examples/tsetlin-machine.ks.lua
@@ -2,161 +2,163 @@
 
 import [ floor, random ]: Math
 
-new-machine: [ numClasses, numClauses, numFeatures, numStates, s, threshold ] ->
-    let taState: array[]
-    let clauseSign: array[]
+new-machine: [ num-classes, num-clauses, num-features, num-states, s, threshold ] ->
+    let ta-state: array[]
+    let clause-sign: array[]
     
-    let clauseCount:       array.from([ length: numClasses ]).fill(0)
-    let clauseOutput:      array.from([ length: numClauses ]).fill(0)
-    let classSum:          array.from([ length: numClasses ]).fill(0)
-    let feedBackToClauses: array.from([ length: numClauses ]).fill(0)
+    let clause-count:        array(num-classes).fill(0)
+    let clause-output:       array(num-clauses).fill(0)
+    let class-sum:           array(num-classes).fill(0)
+    let feedback-to-clauses: array(num-clauses).fill(0)
     
-    initTaState: () *->
-        loop 0 ..< numClauses as i
-            set taState.(i): array[]
-            loop 0 ..< numFeatures as j
-                set taState.(i).(j): array[]
-                set taState.(i).(j).0: if floor(random()) then numStates else numStates + 1
-                set taState.(i).(j).1: if floor(random()) then numStates else numStates + 1
+    init-ta-state: () *->
+        loop 0 ..< num-clauses as i
+            set ta-state.(i): array[]
+            loop 0 ..< num-features as j
+                set ta-state.(i).(j): array[]
+                set ta-state.(i).(j).0: if floor(random()) then num-states else num-states + 1
+                set ta-state.(i).(j).1: if floor(random()) then num-states else num-states + 1
     
-    initClauseSigns: () *->
-        loop 0 ..< numClasses as i
-            set clauseSign.(i): array[]
-            loop 0 ..< (numClauses / numClasses) as j
-                set clauseSign.(i).(j): array[]
-                set clauseSign.(i).(j).0: 0
-                set clauseSign.(i).(j).1: 0
+    init-clause-signs: () *->
+        loop 0 ..< num-classes as i
+            set clause-sign.(i): array[]
+            loop 0 ..< (num-clauses / num-classes) as j
+                set clause-sign.(i).(j): array[]
+                set clause-sign.(i).(j).0: 0
+                set clause-sign.(i).(j).1: 0
     
-    initClauseCount: () *->
-        loop 0 ..< numClasses as i
-            loop 0 ..< floor(numClauses / numClasses) as j
-                set clauseSign.(i).( clauseCount.(i) ).0: i * (numClauses / numClasses) + j
+    init-clause-count: () *->
+        loop 0 ..< num-classes as i
+            loop 0 ..< floor(num-clauses / num-classes) as j
+                set clause-sign.(i).( clause-count.(i) ).0: i * (num-clauses / num-classes) + j
                 if j rem 2 = 0
-                    set clauseSign.(i).( clauseCount.(i) ).1: 1
+                    set clause-sign.(i).( clause-count.(i) ).1: 1
                 else
-                    set clauseSign.(i).( clauseCount.(i) ).1: -1
-                increment clauseCount.(i)
+                    set clause-sign.(i).( clause-count.(i) ).1: -1
+                increment clause-count.(i)
     
-    action: (state) -> if state <= numStates then 0 else 1
+    action: (state) -> if state <= num-states then 0 else 1
     
-    calcClauseOutput: (X, predict ? 0) *->
-        loop 0 ..< numClauses as j
-            set clauseOutput.(j): 1
-            let allExclude: 1
-            loop 0 ..< numFeatures as k
-                actionInclude: action taState.(j).(k).0
-                actionIncludeNeg: action taState.(j).(k).1
-                if actionInclude = 1 or actionIncludeNeg = 1
-                    set allExclude: 0
-                if (actionInclude = 1 and X.(k) = 0) or (actionIncludeNeg = 1 and X.(k) = 1)
-                    set clauseOutput.(j): 0
+    calc-clause-output: (x, predict ? 0) *->
+        loop 0 ..< num-clauses as j
+            set clause-output.(j): 1
+            let all-exclude: 1
+            loop 0 ..< num-features as k
+                action-include: action ta-state.(j).(k).0
+                action-include-neg: action ta-state.(j).(k).1
+                if action-include = 1 or action-include-neg = 1
+                    set all-exclude: 0
+                if (action-include = 1 and x.(k) = 0) or (action-include-neg = 1 and x.(k) = 1)
+                    set clause-output.(j): 0
                     break
-            if predict = 1 and allExclude = 1
-                set clauseOutput.(j): 0
+            if predict = 1 and all-exclude = 1
+                set clause-output.(j): 0
     
-    sumUpClassVotes: () *->
-        loop 0 ..< numClasses as targetClass
-            set classSum.(targetClass): 0
-            loop 0 ..< clauseCount.(targetClass) as j
-                set classSum.(targetClass): _ + (clauseOutput.( clauseSign.(targetClass).(j).0 ) * clauseSign.(targetClass).(j).1)
-                if classSum.(targetClass) > threshold
-                    set classSum.(targetClass): threshold
-                else if classSum.(targetClass) < -threshold
-                    set classSum.(targetClass): -threshold
+    sum-up-class-votes: () *->
+        loop 0 ..< num-classes as target-class
+            set class-sum.(target-class): 0
+            loop 0 ..< clause-count.(target-class) as j
+                set class-sum.(target-class): _
+                    + (clause-output.( clause-sign.(target-class).(j).0 )
+                    * clause-sign.(target-class).(j).1)
+                if class-sum.(target-class) > threshold
+                    set class-sum.(target-class): threshold
+                else if class-sum.(target-class) < -threshold
+                    set class-sum.(target-class): -threshold
     
-    predict: (X) ->
-        calcClauseOutput(X, 1)
-        sumUpClassVotes()
-        let maxClassSum: classSum.0
-        let maxClass: 0
-        loop 1 ..< numClasses as targetClass
-            if maxClassSum < classSum.(targetClass)
-                set maxClassSum: classSum.(targetClass)
-                set maxClass: targetClass
-        maxClass
+    predict: (x) ->
+        calc-clause-output(x, 1)
+        sum-up-class-votes()
+        let max-class-sum: class-sum.0
+        let max-class: 0
+        loop 1 ..< num-classes as target-class
+            if max-class-sum < class-sum.(target-class)
+                set max-class-sum: class-sum.(target-class)
+                set max-class: target-class
+        max-class
     
-    update: (X, targetClass) *->
+    update: (x, target-class) *->
         -- Randomly pick one of the other classes, for pairwise learning of class output
-        let negativeTargetClass: floor(numClasses * (1.0 - 1e-15) * random())
-        while negativeTargetClass = targetClass
-            set negativeTargetClass: floor(numClasses * (1.0 - 1e-15) * random())
+        let negative-target-class: floor(num-classes * (1.0 - 1e-15) * random())
+        while negative-target-class = target-class
+            set negative-target-class: floor(num-classes * (1.0 - 1e-15) * random())
         -- Calculate Clause Output
-        calcClauseOutput(X)
+        calc-clause-output(x)
         -- sum up clause votes
-        sumUpClassVotes()
+        sum-up-class-votes()
         -- calculate Feedback to Clauses
         
-        loop 0 ..< numClauses as j  --  init feedback to clauses
-            set feedBackToClauses.(j): 0
+        loop 0 ..< num-clauses as j  --  init feedback to clauses
+            set feedback-to-clauses.(j): 0
     
-        loop 0 ..< clauseCount.(targetClass) as j
-            if random() > (1.0 / threshold * 2) * (threshold - classSum.(targetClass))
+        loop 0 ..< clause-count.(target-class) as j
+            if random() > (1.0 / threshold * 2) * (threshold - class-sum.(target-class))
                 continue
     
-            if clauseSign.(targetClass).(j).1 > 0
-                increment feedBackToClauses.( clauseSign.(targetClass).(j).0 )
-            else if clauseSign.(targetClass).(j).1 < 0
-                decrement feedBackToClauses.( clauseSign.(targetClass).(j).0 )
+            if clause-sign.(target-class).(j).1 > 0
+                increment feedback-to-clauses.( clause-sign.(target-class).(j).0 )
+            else if clause-sign.(target-class).(j).1 < 0
+                decrement feedback-to-clauses.( clause-sign.(target-class).(j).0 )
     
-        loop 0 ..< clauseCount.(negativeTargetClass) as j
-            if random() > (1.0 / threshold * 2) * (threshold + classSum.(negativeTargetClass))
+        loop 0 ..< clause-count.(negative-target-class) as j
+            if random() > (1.0 / threshold * 2) * (threshold + class-sum.(negative-target-class))
                 continue
     
-            if clauseSign.(negativeTargetClass).(j).1 > 0
-                decrement feedBackToClauses.( clauseSign.(negativeTargetClass).(j).0 )
-            else if clauseSign.(negativeTargetClass).(j).1 < 0)
-                increment feedBackToClauses.( clauseSign.(negativeTargetClass).(j).0 )
+            if clause-sign.(negative-target-class).(j).1 > 0
+                decrement feedback-to-clauses.( clause-sign.(negative-target-class).(j).0 )
+            else if clause-sign.(negative-target-class).(j).1 < 0)
+                increment feedback-to-clauses.( clause-sign.(negative-target-class).(j).0 )
     
         -- Train individual Automata
-        loop 0 ..< numClauses as j
-            if feedBackToClauses.(j) > 0
+        loop 0 ..< num-clauses as j
+            if feedback-to-clauses.(j) > 0
                 -- Type I Feedback (Combats False Negatives)
-                if clauseOutput.(j) = 0
-                    loop 0 ..< numFeatures as k
+                if clause-output.(j) = 0
+                    loop 0 ..< num-features as k
                         if random() <= 1.0 / s
-                            if taState.(j).(k).0 > 1
-                                decrement taState.(j).(k).0
+                            if ta-state.(j).(k).0 > 1
+                                decrement ta-state.(j).(k).0
                         if random() <= 1.0 / s
-                            if taState.(j).(k).1 > 1
-                                decrement taState.(j).(k).1
-                else if clauseOutput.(j) = 1
-                    loop 0 ..< numFeatures as k
-                        if X.(k) = 1
+                            if ta-state.(j).(k).1 > 1
+                                decrement ta-state.(j).(k).1
+                else if clause-output.(j) = 1
+                    loop 0 ..< num-features as k
+                        if x.(k) = 1
                             if random() <= (s - 1) / s
-                                if taState.(j).(k).0 < numStates * 2
-                                    increment taState.(j).(k).0
+                                if ta-state.(j).(k).0 < num-states * 2
+                                    increment ta-state.(j).(k).0
                             if random() <= 1.0 / s
-                                if taState.(j).(k).1 > 1
-                                    decrement taState.(j).(k).1
-                        else if X.(k) = 0
+                                if ta-state.(j).(k).1 > 1
+                                    decrement ta-state.(j).(k).1
+                        else if x.(k) = 0
                             if random() <= (s - 1) / s
-                                if taState.(j).(k).1 < numStates * 2
-                                    increment taState.(j).(k).1
+                                if ta-state.(j).(k).1 < num-states * 2
+                                    increment ta-state.(j).(k).1
                             if random() <= 1.0 / s
-                                if taState.(j).(k).0 > 1
-                                    decrement taState.(j).(k).0
-            else if feedBackToClauses.(j) < 0
+                                if ta-state.(j).(k).0 > 1
+                                    decrement ta-state.(j).(k).0
+            else if feedback-to-clauses.(j) < 0
                 --  Type II Feedback (Combats False Positives)
-                if clauseOutput.(j) = 1
-                    loop 0 ..< numFeatures as k
-                        let actionInclude: action taState.(j).(k).0
-                        let actionIncludeNegated: action taState.(j).(k).1
-                        if X.(k) = 0
-                            if actionInclude = 0 and taState.(j).(k).0 < numStates * 2
-                                increment taState.(j).(k).0
-                        else if X.(k) = 1
-                            if actionIncludeNegated = 0 and taState.(j).(k).1 < numStates * 2
-                                increment taState.(j).(k).1
+                if clause-output.(j) = 1
+                    loop 0 ..< num-features as k
+                        let action-include: action ta-state.(j).(k).0
+                        let action-include-negated: action ta-state.(j).(k).1
+                        if x.(k) = 0
+                            if action-include = 0 and ta-state.(j).(k).0 < num-states * 2
+                                increment ta-state.(j).(k).0
+                        else if x.(k) = 1
+                            if action-include-negated = 0 and ta-state.(j).(k).1 < num-states * 2
+                                increment ta-state.(j).(k).1
     
-    evaluate: (X, y) ->
+    evaluate: (x, y) ->
         let errors: 0
-        loop 0 ..< X.length as l
-            if predict X.(l) ≠ y.(l) then increment errors
-        1.0 - errors / X.length
+        loop 0 ..< x.length as l
+            if predict x.(l) ≠ y.(l) then increment errors
+        1.0 - errors / x.length
     
-    initTaState()
-    initClauseSigns()
-    initClauseCount()
+    init-ta-state()
+    init-clause-signs()
+    init-clause-count()
     
     [ predict, update, evaluate ]
     
